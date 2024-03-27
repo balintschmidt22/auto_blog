2dc40ec55c9387cefbb6b3cd7f748828f70b2079
Schmidt Bálint Márk <frjr89@inf.elte.hu>
2024-03-27 01:11:41 +0100

    Fixing DatabaseTest

--- tests/Feature/DatabaseTest.php
+++ tests/Feature/DatabaseTest.php
@@ -8,17 +8,17 @@ use Tests\TestCase;
 
 class DatabaseTest extends TestCase
 {
-    public function admin_exists(): void
+    public function test_admin_exists(): void
     {
         $this->assertDatabaseHas('users', ['role' => 'adm']);
     }
 
-    public function moderator_exists(): void
+    public function test_moderator_exists(): void
     {
         $this->assertDatabaseHas('users', ['role' => 'mod']);
     }
 
-    public function user_exists(): void
+    public function test_user_exists(): void
     {
         $this->assertDatabaseHas('users', ['role' => 'usr']);
     }
